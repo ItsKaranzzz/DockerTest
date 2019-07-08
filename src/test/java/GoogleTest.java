1ba60e5d06e78c47d08c989e1326bd9a17428647
Karan Chaudhary <karanc@Karans-MacBook-Pro.local>
2019-07-08 22:33:52 +0530

    Karan|Updated the suite with the docker-compose yaml file setup

--- src/test/java/GoogleTest.java
+++ src/test/java/GoogleTest.java
@@ -1,32 +1,39 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.pagefactory.ByAll;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.*;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class GoogleTest {
     WebDriver driver;
     DesiredCapabilities cap;
+    String URL = Utils.getConfigs().getProperty("Test_Url");
 
     @Parameters({"browser"})
     @BeforeTest
-    public void setUp(String browser) {
+    public void setUp(String browser) throws MalformedURLException {
         if (browser.equalsIgnoreCase("chrome")) {
             cap = new DesiredCapabilities();
             cap.setBrowserName("chrome");
             cap.setPlatform(Platform.LINUX);
             WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
+            driver = new RemoteWebDriver(new URL("http://0.0.0.0:4444/wd/hub"), cap);
         } else if (browser.equalsIgnoreCase("firefox")) {
             cap = new DesiredCapabilities();
             cap.setBrowserName("firefox");
             cap.setPlatform(Platform.LINUX);
             WebDriverManager.firefoxdriver().setup();
-            driver = new FirefoxDriver();
+            driver = new RemoteWebDriver(new URL("http://0.0.0.0:4444/wd/hub"), cap);
         }
 
 
@@ -34,15 +41,23 @@ public class GoogleTest {
 
     @Test(threadPoolSize = 2)
     public void verifyTitle() {
-        // String URL = Utils.getConfigs().getProperty("Test_Url");
-        driver.get("http://www.google.com");
-
+        driver.get(URL);
         String actualTitle = driver.getTitle();
         String expectedTitle = "Google";
         Assert.assertEquals(actualTitle, expectedTitle, "Title was not " + expectedTitle);
     }
 
-    @AfterMethod
+    @Test
+    public void verifySearchedLanguageOptions() {
+        driver.get(URL);
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+        WebElement searchBtn = driver.findElement(By.id("SIvCob"));
+        wait.until(ExpectedConditions.elementToBeClickable(searchBtn));
+        String actualSearchBtnText = searchBtn.getText();
+        Assert.assertEquals(actualSearchBtnText, "Google offered in: हिन्दी বাংলা తెలుగు मराठी தமிழ் ગુજરાતી ಕನ್ನಡ മലയാളം ਪੰਜਾਬੀ", "Search Languages shown didn't match");
+    }
+
+    @AfterTest
     public void tearDown() {
         driver.quit();
     }
